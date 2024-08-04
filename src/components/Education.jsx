c9b7b3c194edbf2dce7c6541f87252bb02cf7e01
Biswajit Ghosal <biswajit.development@gmail.com>
2024-08-04 22:55:04 +0530

    chore: Update education component with new details

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,40 +2,48 @@ import React from 'react';
 
 const educationalData = [
     {
-        title: 'Bachelor of Science in Computer Science',
-        institution: 'University of Example',
-        graduationDate: 'May 2020',
-        description: 'Focused on software engineering, algorithms, and data structures.'
+        title: 'B.Tech in information Technology',
+        institution: 'Meghnad Saha Institute of Technology',
+        university: 'MAKAUT',
+        graduationDate: 'July 2023',
+        result: '9.29 CGPA'
     },
     {
-        title: 'Master of Science in Artificial Intelligence',
-        institution: 'Institute of Example',
-        graduationDate: 'May 2022',
-        description: 'Specialized in machine learning, deep learning, and natural language processing.'
+        title: 'Higher Secondary(12th)',
+        institution: 'Bankura Municipal High School',
+        university: 'WBCHSE',
+        graduationDate: 'May 2018',
+        result: '86%'
     },
     {
-        title: 'Certification in Data Science',
-        institution: 'Online Platform Example',
-        graduationDate: 'March 2023',
-        description: 'Covered data analysis, visualization, and statistical modeling.'
+        title: 'Madhyamik(10th)',
+        institution: 'Ramharipur Ramkrishna Mission High School',
+        university: 'WBBSE',
+        graduationDate: 'March 2016',
+        result: '88.71%'
     }
 ];
 
 function Education() {
     return (
         <div className="container mx-auto p-6">
-            <h1 className="text-4xl font-bold text-center mb-8">Education</h1>
+            <h1 className="text-5xl font-extrabold text-center mb-10 pb-5 border-b-4 border-indigo-500 text-gray-800 ">
+                Educational Details
+            </h1>
             <div className="relative">
-                <div className="border-l-4 border-blue-500 absolute h-full top-0 left-1/2 transform -translate-x-1/2"></div>
+                <div className="border-l-4 border-blue-500 absolute h-full top-0 left-3/4 transform -translate-x-1/2"></div>
+                <div className="border-l-4 border-blue-500 absolute h-full top-0 left-1/4 transform -translate-x-1/2"></div>
                 <div className="space-y-8">
                     {educationalData.map((data, index) => (
                         <div key={index} className="relative flex items-center">
-                            <div className="w-6 h-6 bg-blue-500 rounded-full absolute left-1/2 transform -translate-x-1/2 border-4 border-white"></div>
-                            <div className="bg-white shadow-lg rounded-lg p-6 w-1/2 ml-8">
+                            <div className="w-6 h-6 bg-blue-500 rounded-full absolute left-1/4 transform -translate-x-1/2 border-4 border-white"></div>
+                            <div className="w-6 h-6 bg-blue-500 rounded-full absolute left-3/4 transform -translate-x-1/2 border-4 border-white"></div>
+                            <div className="bg-red-100 shadow-lg rounded-lg p-6 w-full text-center">
                                 <h2 className="text-2xl font-semibold mb-2">{data.title}</h2>
                                 <h3 className="text-xl font-medium text-gray-600 mb-2">{data.institution}</h3>
-                                <p className="text-gray-700 mb-4">Graduated: {data.graduationDate}</p>
-                                <p className="text-gray-600">{data.description}</p>
+                                <h4 className="text-xl font-medium text-gray-600 mb-2">University/Board: {data.university}</h4>
+                                <p className="text-gray-700 mb-4">Graduated On: {data.graduationDate}</p>
+                                <p className="text-gray-600">Marks: {data.result}</p>
                             </div>
                         </div>
                     ))}
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,35 +1,123 @@
 // src/components/Timeline.js
-import React from 'react';
+import React, { useState } from 'react';
+
+// Import Certificates
+import ExavaluCertificate from '../assets/files/internship/Exavalu_Certificate.jpg';
+import PersistentCertificate from '../assets/files/internship/Persistent_Certificate.png';
+import ASDCertificate from '../assets/files/internship/ASD_Certificate.png';
+
+
+// Import Logos
+import ASD from '../assets/img/experience/ASD.png';
+import PersistentLogo from '../assets/img/experience/persistent-systems-logo.png';
+import ExavaluLogo from '../assets/img/experience/Exavalu Logo.png';
+
 
 const experiences = [
     {
-        date: 'January 2020',
-        title: 'Started learning React',
-        description: 'Began my journey into the world of React and built my first application.',
+        organizationLogo: ExavaluLogo,
+        organization: 'Exavalu',
+        date: 'October 2023 - Present',
+        title: 'Associate Developer',
+        description: 'Working on GenAI, LLM',
+        certificate: null,
+        skills: ['React', 'Node.js', 'Express', 'MongoDB', 'GenAi', 'LLMOps', 'ML']
     },
     {
-        date: 'June 2020',
-        title: 'First Freelance Project',
-        description: 'Completed a freelance project using React and Tailwind CSS.',
+        organizationLogo: ExavaluLogo,
+        organization: 'Exavalu',
+        date: 'January 2023 - September 2023',
+        title: 'Intern',
+        description: 'During this time I have learned JAVA Enterprise Edition along with Web Development tools like HTML, CSS, JAVASCRIPT, BOOTSTRAP . In the end of this phase I have developed a web development project with a team of 6 people where we have made a Pharmacy Management web application using MYSQL database.',
+        certificate: ExavaluCertificate,
+        skills: ["GitHub", "MVC", "Bootstrap", "Struts", "JSP", "JDBC", "MySQL", "JavaEE", "DBMS", "CSS", "JavaScript"]
+
     },
-    // Add more experiences as needed
+    {
+        organizationLogo: PersistentLogo,
+        organization: 'Persistent Systems',
+        date: 'May 2022 - July 2022',
+        title: 'Intern',
+        description: 'During this internship I have learned basic computer science fundamental, python, operating system, DBMS.',
+        certificate: PersistentCertificate,
+        skills: ['Python', 'OOP', 'DBMS']
+    },
+    {
+        organizationLogo: ASD,
+        organization: 'Academy of Skill Development',
+        date: 'August 2020 - September 2020',
+        title: 'Training',
+        description: 'Developed a responsive website using HTML, CSS, and JavaScript, prioritizing user-centric design for seamless mobile and desktop experiences. Applied CSS Flexbox/Grid for dynamic layouts, and JavaScript for interactive features and form validation. Demonstrates proficiency in front-end technologies, responsive design, and collaborative teamwork.',
+        certificate: ASDCertificate,
+        skills: ['Html', 'CSS', 'JavaScript', 'Bootstrap']
+    }
 ];
 
 const Experience = () => {
+    const [enlargedImageIndex, setEnlargedImageIndex] = useState(null);
+
+    const handleImageClick = (index) => {
+        setEnlargedImageIndex(enlargedImageIndex === index ? null : index);
+    };
+
     return (
         <div className="container mx-auto my-10 p-5">
-            <h1 className="text-3xl font-bold mb-10 text-center">My Experience</h1>
-            <div className="relative border-l border-gray-200">
-                {experiences.map((experience, index) => (
-                    <div className="mb-10 ml-4" key={index}>
-                        <div className="absolute w-3 h-3 bg-blue-500 rounded-full mt-1.5 -left-1.5 border border-white"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-400">{experience.date}</time>
-                        <h3 className="text-lg font-semibold text-gray-900">{experience.title}</h3>
-                        <p className="mb-4 text-base font-normal text-gray-600">{experience.description}</p>
-                    </div>
-                ))}
+            <h1 className="text-5xl font-extrabold text-center mb-10 pb-5 border-b-4 border-indigo-500 text-gray-800 ">
+                Experience Details
+            </h1>
+            <div className="relative">
+                <div className="border-l-4 border-blue-500 absolute h-full top-0 left-3/4 transform -translate-x-1/2"></div>
+                <div className="border-l-4 border-blue-500 absolute h-full top-0 left-1/4 transform -translate-x-1/2"></div>
+                <div className="space-y-8">
+                    {experiences.map((experience, index) => (
+                        <div key={index} className="relative flex items-center">
+                            <div className="w-6 h-6 bg-blue-500 rounded-full absolute left-1/4 transform -translate-x-1/2 border-4 border-white"></div>
+                            <div className="w-6 h-6 bg-blue-500 rounded-full absolute left-3/4 transform -translate-x-1/2 border-4 border-white"></div>
+                            <div className="bg-white rounded-lg w-full text-left">
+                                <div className='flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow-md p-6 mb-6 transition-transform transform'>
+                                    <div className='md:w-2/3 md:pr-6'>
+                                        <time className="mb-1 text-sm font-normal leading-none text-gray-500 flex items-center">
+                                            <img src={experience.organizationLogo} alt={experience.organization} height={50} width={50} className="rounded-full shadow-sm" />
+                                            <span className="ml-2">({experience.date})</span>
+                                        </time>
+                                        <h3 className="text-xl font-semibold text-gray-900 mb-2">{experience.title}</h3>
+                                        <p className="text-base font-normal text-gray-700 mb-4">{experience.description}</p>
+                                        <div className="flex flex-wrap space-x-2">
+                                            {experience.skills.map((skill, index) => (
+                                                <span key={index} className="px-2 py-1 bg-blue-100 text-blue-700 text-sm rounded-md shadow-sm">
+                                                    {skill}
+                                                </span>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <div className='md:w-1/3 mt-4 md:mt-0'>
+                                        {experience.certificate ? (
+                                            <img
+                                                className={`rounded-lg border border-gray-200 shadow-md hover:shadow-lg transition-transform duration-300 ${enlargedImageIndex === index ? 'transform scale-150' : ''}`}
+                                                src={experience.certificate}
+                                                alt={`${experience.organization} Certificate`}
+                                                height={enlargedImageIndex === index ? 800 : 300}
+                                                width={enlargedImageIndex === index ? 1600 : 500}
+                                                onClick={() => handleImageClick(index)}
+                                                style={{ cursor: 'pointer' }}
+                                            />
+                                        ) : (
+                                            <img
+                                                className="rounded-lg border border-gray-200 shadow-md hover:shadow-lg transition-shadow duration-300"
+                                                src={experience.organizationLogo}
+                                                alt={`${experience.organization} Logo`}
+                                                height={300}
+                                                width={500}
+                                            />
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </div >
     );
 };
 
